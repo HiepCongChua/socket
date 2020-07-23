1fa3f77dd4aaed854f6e062180b9bb5ce9a2840f
HiepCongChua <hieplevuc@gmail.com>
2020-07-23 14:03:38 +0700

    update api

--- basic-socket.io/basics.js
+++ basic-socket.io/basics.js
@@ -1,13 +1,16 @@
-const http = require('http');
-const socketio = require('socket.io');
-const server = http.createServer((req,res)=>{
-    res.end("I am connected!");
-});
-const io = socketio(server);
-io.on('connection',(socket,req)=>{
-  socket.emit('welcome','Welcome to the websocket server!');
-  socket.on('message',(msg)=>{
-     console.log(msg);
-  });
-});
-server.listen(8080);
+const http = require("http")
+const socketio = require("socket.io")
+const server = http.createServer((req, res) => {
+  // Tao sever HTTP
+  res.end("I am connected!")
+})
+const io = socketio(server) // Attachment socker server to http
+io.on("connection", (socket, req) => {
+  socket.emit("welcome", "Welcome to the websocket server!")
+  socket.on("message", (msg) => {
+    console.log("OKKOKo")
+    msg("abc")
+    // console.log(msg)
+  })
+})
+server.listen(8080)
